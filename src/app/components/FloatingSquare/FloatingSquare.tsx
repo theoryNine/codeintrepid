71dec442dbd99f014654d1037bf526fe80435974
Ansel <anselcolvin@gmail.com>
2025-01-25 16:56:41 -0800

    adding background shapes

--- src/app/components/FloatingSquare/FloatingSquare.tsx
+++ src/app/components/FloatingSquare/FloatingSquare.tsx
@@ -10,12 +10,12 @@ export default function FloatingSquare({
 }: FloatingSquareProps): ReactElement {
   const positionClass =
     location === "top"
-      ? "-top-[100px] -left-[320px] md:-top-[310px] md:-left-[320px]"
-      : "bottom-0 -right-[160px] md:bottom-[-310px] md:-right-[320px]";
+      ? "-top-[200px] -left-[210px] lg:-top-[310px] lg:-left-[320px]"
+      : "bottom-[-200px] -right-[210px] lg:bottom-[-310px] lg:-right-[320px]";
 
   return (
     <div
-      className={`w-[300px] h-[300px] md:w-[600px] md:h-[600px] absolute bg-cover bg-[url('/space.jpg')] ${positionClass} rotate-45 saturate-200 border-2 border-white`}
+      className={`w-[300px] h-[300px] lg:w-[600px] lg:h-[600px] absolute bg-cover bg-[url('/space.jpg')] ${positionClass} rotate-45 saturate-200 border-2 border-white`}
     />
   );
 }
