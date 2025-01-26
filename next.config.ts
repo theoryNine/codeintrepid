78f4c3ed5ae7de7e49b78ac21792d9d13b3ab631
Ansel <anselcolvin@gmail.com>
2025-01-25 17:04:35 -0800

    add build caching

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  experimental: {
+    largePageDataBytes: 128 * 1000,
+  },
+  reactStrictMode: true,
+  poweredByHeader: false,
+  compress: true,
 };
 
 export default nextConfig;
